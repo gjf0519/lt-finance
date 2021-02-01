c7e789e99edf346c76111d59ae9dc203541a95e7
DEC123123 <jianfeng.gai@pharmacodia.com>
2021-02-01 19:59:39 +0800

    日K周K数据

--- lt-finance-console/src/main/java/com/lt/security/handler/CustomAccessDeniedHandler.java
+++ lt-finance-console/src/main/java/com/lt/security/handler/CustomAccessDeniedHandler.java
@@ -1,12 +1,15 @@
 package com.lt.security.handler;
 
 import com.alibaba.fastjson.JSON;
+import com.lt.common.ConsoleConstants;
 import com.lt.web.ResultCode;
 import com.lt.web.ResultEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,9 +26,14 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request,
                        HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException {
-        log.info("未开通权限:{}",accessDeniedException);
-        ResultCode resultCode = ResultCode.AUTH_403_3;
-        response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(JSON.toJSONString(ResultEntity.fail(resultCode.getCode(),resultCode.getVal())));
+        log.info("权限不足或未登录:{}",accessDeniedException);
+        String access_token = request.getHeader(ConsoleConstants.AUTHENTICATION_HEAD);
+        if(StringUtils.isEmpty(access_token)){
+            response.sendRedirect(request.getContextPath() + "/login.html");
+        }else {
+            ResultCode resultCode = ResultCode.AUTH_403_3;
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(JSON.toJSONString(ResultEntity.fail(resultCode.getCode(),resultCode.getVal())));
+        }
     }
 }
